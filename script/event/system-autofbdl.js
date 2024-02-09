5bd56b88104535aedaf644e27dca05fe488397a7
codebox4chan <lkpanio25@gmail.com>
2024-02-10 07:30:16 +0800

    update

--- script/event/system-autofbdl.js
+++ script/event/system-autofbdl.js
@@ -1,44 +1,48 @@
 const axios = require('axios');
 const fs = require('fs-extra');
-const { fbdl } = require('vihangayt-fbdl');
+const { fbdl2 } = require('vihangayt-fbdl');
 
 module.exports.config = {
   name: "auto-fbdl",
-  type: "Downloader",
-  info: "detects facebook video link and download",
   version: "2.0.0",
+  role: 0,
   credits: "Kenneth Panio",
+  info: "Download Facebook videos",
+  hasPrefix: false,
+  type: "downloader",
   usage: "[url]",
-  cd: 2,
+  cd: 5,
 };
 
 module.exports.handleEvent = async ({ event, api }) => {
-  const { threadID, messageID, senderID } = event;
+  const { threadID, messageID, body } = event;
   const pattern = /https:\/\/www\.facebook\.com\/\S+/;
-
-  const body = event.body ? event.body.toLowerCase() : '';
 
   if (body && pattern.test(body)) {
     try {
       api.sendMessage("FB URL Detected!\nDownloading Video....", threadID, messageID);
 
       const link = body.match(pattern)[0];
-      const response = await fbdl(link);
+      const response = await fbdl2(link);
 
-      const videoBuffer = await axios.get(response.result.HD, { responseType: 'arraybuffer' });
+      if (response.status && response.result && response.result.HD) {
+        const videoBuffer = await axios.get(response.result.HD, { responseType: 'arraybuffer' });
 
-      const path = __dirname + `/cache/fbdl.mp4`;
-      fs.writeFileSync(path, Buffer.from(videoBuffer.data, 'binary'));
+        const path = __dirname + `/cache/fbdl.mp4`;
+        fs.writeFileSync(path, Buffer.from(videoBuffer.data, 'binary'));
 
-      api.sendMessage(
-        {
-          body: "Downloaded Success!",
-          attachment: fs.createReadStream(path),
-        },
-        threadID,
-        () => fs.unlinkSync(path),
-        messageID
-      );
+        api.sendMessage(
+          {
+            body: `Downloaded Success! Title: ${response.result.Title}`,
+            attachment: fs.createReadStream(path),
+          },
+          threadID,
+          () => fs.unlinkSync(path),
+          messageID
+        );
+      } else {
+        throw new Error("Invalid response from fbdl");
+      }
     } catch (error) {
       console.error("Failed to Download video!", error.message);
       api.sendMessage("Failed to send video, the link might not be supported or the API is unavailable.", threadID, messageID);
