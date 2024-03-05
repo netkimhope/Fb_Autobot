ce4607582a958de6196bd72bb40afa39c1c4077c
codebox4chan <lkpanio25@gmail.com>
2024-03-05 14:19:52 +0800

    update

--- script/guide-help.js
+++ script/guide-help.js
@@ -4,7 +4,7 @@ module.exports.config = {
   role: 0,
   aliases: ['info'],
   info: "Beginner's guide",
-  usage: "[page] or [command]",
+  usage: "[Page-Number] or [Command Name]",
   credits: 'Developer',
 };
 
@@ -15,50 +15,50 @@ module.exports.run = async function ({ api, event, enableCommands, args, Utils, 
   try {
     if (!input) {
       const pages = 20, page = 1, start = (page - 1) * pages, end = start + pages;
-      let helpMessage = `📚 | 𝗖𝗠𝗗 𝗟𝗜𝗦𝗧: 〔${prefix}〕\n\n`;
+        let helpMessage = `📚 | 𝗖𝗠𝗗 𝗟𝗜𝗦𝗧: 〔${prefix || 'ɴᴏ ᴘʀᴇғɪx'}〕\n\n`;
 
       for (let i = start; i < Math.min(end, commands.length); i++) {
         const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(commands[i].toLowerCase()))?.[1];
 
         if (command) {
           const { name, info, usage } = command;
-          helpMessage += `\t${i + 1}. ►〔 ${name} 〕 ${usage ? `${usage}` : ''}\n- ${info}\n\n`;
+          helpMessage += `\t${i + 1}. ${name} ${usage ? `${usage}` : ''}\n`;
         }
       }
 
-      helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help page number'. To view information about a specific command, type '${prefix}help command name'.`;
+      helpMessage += `\nᴘᴀɢᴇ: ${page}/${Math.ceil(commands.length / pages)}. ᴛᴏ ᴠɪᴇᴡ ɴᴇxᴛ ᴘᴀɢᴇ, ᴛʏᴘᴇ '${prefix}ʜᴇʟᴘ [ᴘᴀɢᴇ-ɴᴜᴍʙᴇʀ]'. ᴛᴏ ᴠɪᴇᴡ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴄᴏᴍᴍᴀɴᴅ, ᴛʏᴘᴇ '${prefix}ʜᴇʟᴘ [ɴᴀᴍᴇ ᴏғ ᴄᴏᴍᴍᴀɴᴅ]'.`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else if (!isNaN(input)) {
       const page = parseInt(input), pages = 20, start = (page - 1) * pages, end = start + pages;
-      let helpMessage = `📚 | 𝗖𝗠𝗗 𝗟𝗜𝗦𝗧: 〔${prefix}〕\n\n`;
+      let helpMessage = `📚 | 𝗖𝗠𝗗 𝗟𝗜𝗦𝗧: 〔${prefix || 'ɴᴏ ᴘʀᴇғɪx'}〕\n\n`;
 
       for (let i = start; i < Math.min(end, commands.length); i++) {
         const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(commands[i].toLowerCase()))?.[1];
 
         if (command) {
           const { name, info, usage } = command;
-          helpMessage += `\t${i + 1}. ►〔 ${name} 〕 ${usage ? `${usage}` : ''}\n- ${info}\n\n`;
+          helpMessage += `\t${i + 1}. ${name} ${usage ? `${usage}` : ''}\n`;
         }
       }
 
-      helpMessage += `\nPage ${page} - ${Math.ceil(commands.length / pages)}`;
+      helpMessage += `\nᴘᴀɢᴇ ${page} - ${Math.ceil(commands.length / pages)}`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else {
       const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input?.toLowerCase()))?.[1];
 
       if (command) {
         const { name, version, role, aliases = [], info, usage, credits, cd, hasPrefix } = command;
-        const roleMessage = role !== undefined ? (role === 0 ? '➛ Permission: user' : (role === 1 ? '➛ Permission: admin' : (role === 2 ? '➛ Permission: thread Admin' : (role === 3 ? '➛ Permission: super Admin' : '')))) : '';
-        const aliasesMessage = aliases.length ? `➛ Aliases: ${aliases.join(', ')}\n` : '';
-        const descriptionMessage = info ? `Description: ${info}\n` : '';
-        const usageMessage = usage ? `➛ Usage: ${usage}\n` : '';
-        const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
-        const versionMessage = version ? `➛ Version: ${version}\n` : '';
-        const cooldownMessage = cd ? `➛ Cooldown: ${cd} second(s)\n` : '';
-        const message = ` 「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
+        const roleMessage = role !== undefined ? (role === 0 ? 'ʀᴏʟᴇ: ᴜsᴇʀ' : (role === 1 ? 'ʀᴏʟᴇ: ʙᴏᴛ-ᴀᴅᴍɪɴ ᴏᴡɴᴇʀ' : (role === 2 ? 'ʀᴏʟᴇ: ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs' : (role === 3 ? 'ʀᴏʟᴇ: sᴜᴘᴇʀ ᴀᴅᴍɪɴs/ᴍᴏᴅᴇʀᴀᴛᴏʀs' : '')))) : '';
+        const aliasesMessage = aliases.length ? `➛ ᴀʟɪᴀsᴇs: ${aliases.join(', ')}\n` : '';
+        const descriptionMessage = info ? `ɪɴғᴏ: ${info}\n` : '';
+        const usageMessage = usage ? `ᴜsᴀɢᴇ: ${usage}\n` : '';
+        const creditsMessage = credits ? `ᴀᴜᴛʜᴏʀ: ${credits}\n` : '';
+        const versionMessage = version ? `ᴠᴇʀsɪᴏɴ: ${version}\n` : '';
+        const cooldownMessage = cd ? `ᴄᴏᴏʟᴅᴏᴡɴ: ${cd} second(s)\n` : '';
+        const message = `𝗖𝗢𝗠𝗠𝗔𝗡𝗗\n\nɴᴀᴍᴇ: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
         api.sendMessage(message, event.threadID, event.messageID);
       } else {
-        api.sendMessage('Command not found.', event.threadID, event.messageID);
+        api.sendMessage(`ᴄᴏᴍᴍᴀɴᴅ ᴅᴏᴇs'ɴᴛ ᴇxɪsᴛ!`, event.threadID, event.messageID);
       }
     }
   } catch (error) {
