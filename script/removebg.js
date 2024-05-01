f3fc239fe300c1fa0fbe8d4395b0612a03057ea2
かいと <160225758+unknown317@users.noreply.github.com>
2024-05-01 13:39:57 +0800

    Update removebg.js

--- script/removebg.js
+++ script/removebg.js
@@ -21,7 +21,7 @@ module.exports.run = async ({ api, event, args }) => {
 
   try {
     api.sendMessage("Removing background...", threadID, messageID);
-    const response = await axios.get(`https://combineapi-7fa2b2874c53.herokuapp.com/api/try/removebg?url=${encodeURIComponent(mark)}`);
+    const response = await axios.get(`https://allinoneapis-8q2m.onrender.com/api/try/removebg?url=${encodeURIComponent(mark)}`);
     const processedImageURL = response.data.image_data;
 
     const img = (await axios.get(processedImageURL, { responseType: "arraybuffer"})).data;
