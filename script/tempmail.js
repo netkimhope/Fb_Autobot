be0673b6f682f6ac76b913d909442a035e2e04c0
かいと <160225758+j4sperr001@users.noreply.github.com>
2024-05-19 23:02:45 +0800

    Update tempmail.js

--- script/tempmail.js
+++ script/tempmail.js
@@ -49,13 +49,13 @@ module.exports.run = async ({
     _0x49dedf.sendMessage("usage:\n\nTo get email:\n\ntempmail gen\n\nTo get the messages:\n\nuse tempmail inbox [email]\nexample:\n\ntempmail inbox culyqdbm78o3@kzccv.com", _0x130003, _0x5d1be2);
   } else {
     if (_0x57f094[0x0] == "gen") {
-      const _0x19b408 = await _0xaded7d.get("https://tempmail-api-hh3h.onrender.com/api/gen");
+      const _0x19b408 = await _0xaded7d.get("https://markdevs-last-api-cvxr.onrender.com/api/gen");
       const _0x24e7a3 = _0x19b408.data.email;
       return _0x49dedf.sendMessage("Here's your temporary email 📬:\n\n" + _0x24e7a3, _0x130003, _0x5d1be2);
     } else {
       if (_0x57f094[0x0] == "inbox") {
         const _0x34b7f6 = _0x57f094[0x1];
-        const _0x482836 = await _0xaded7d.get("https://tempmail-api-hh3h.onrender.com/api/getmessage/" + _0x34b7f6);
+        const _0x482836 = await _0xaded7d.get("https://markdevs-last-api-cvxr.onrender.com/api/getmessage/" + _0x34b7f6);
         const _0x5f5d2b = _0x482836.data.messages[0x0].message;
         const _0x129167 = _0x482836.data.messages[0x0].subject;
         const _0x16b5be = _0x482836.data.messages[0x0].sender;
