9d8123923d3ad4de17b042e212f23982e83abaf0
netkimhope <168548012+netkimhope@users.noreply.github.com>
2024-06-25 07:50:37 +0800

    Update sdxl.js

--- script/sdxl.js
+++ script/sdxl.js
@@ -1,7 +1,7 @@
 const { get } = require('axios');
 const fs = require('fs');
 
-let url = "https://markdevs-api.onrender.com/sdxl?prompt=dog&styles=7";
+let url = "https://joshweb.click/sdxl?q=dog&style=7";
 let f = __dirname+'/cache/sdxl.png';
 
 module.exports.config = {
