eec94a26e8a630e4b2551bdb6d758e850c70dd77
netkimhope <168548012+netkimhope@users.noreply.github.com>
2024-06-25 07:42:43 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,5 +1,5 @@
 const {get} = require('axios');
-const url = "https://markdevs-api.onrender.com/gpt3?prompt=&uid=";
+const url = "https://joshweb.click/gpt4?prompt=hi&uid=100";
 module.exports = {
     config: {
        name: "ai",
