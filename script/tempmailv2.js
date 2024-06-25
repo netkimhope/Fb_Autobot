4a689b9cc94b179f8e370e6824c3cb03199a76f5
netkimhope <168548012+netkimhope@users.noreply.github.com>
2024-06-25 21:35:43 +0800

    Update tempmailv2.js

--- script/tempmailv2.js
+++ script/tempmailv2.js
@@ -2,13 +2,13 @@ const axios = require('axios');
 const { TempMail } = require("1secmail-api");
 
 module.exports.config = {
-  name: "tempmailv2",
+  name: "tempmail",
   version: "1.0.1",
   info: "Generates random email from www.1secmail.com and fetches message from inbox",
   credits: "Kenneth Panio",
   type: "Accounting",
   role: 0,
-  aliases: ['1secmail'],
+  aliases: ['1secmail', 'temp', 'genmail', 'dumpmail', 'mail', 'dump'],
   usage: "[count (optional)] or inbox [email] [message limit to show (optional)]"
 };
 
@@ -28,11 +28,11 @@ function generateRandomId() {
   return randomId;
 }
 
-module.exports.run = async ({ api, event, args }) => {
+module.exports.run = async function ({ api, event, args, chat }) {
   try {
     if (args[0] === 'inbox') {
       if (!args[1]) {
-        return api.sendMessage("Please provide an email address for the inbox.", event.threadID);
+        return chat.reply("Please provide an email address for the inbox.");
       }
 
       const [username, domain] = args[1].replace(/\(\.\)/g, '.').split('@');
@@ -40,7 +40,7 @@ module.exports.run = async ({ api, event, args }) => {
       const messages = inboxResponse.data;
 
       if (!messages || messages.length === 0) {
-        return api.sendMessage(`No messages found for ${args[1]}.`, event.threadID);
+        return chat.replyID(`No messages found for ${args[1]}.`);
       }
 
       const displayLimit = args[2] || DEFAULT_DISPLAY_LIMIT;
@@ -55,18 +55,20 @@ module.exports.run = async ({ api, event, args }) => {
         messageText += `👤 𝗦𝗘𝗡𝗗𝗘𝗥: ${detailedMessage.from}\n🎯 𝗦𝗨𝗕𝗝𝗘𝗖𝗧: ${detailedMessage.subject || 'No Subject 🎯'}\n📅 𝗗𝗔𝗧𝗘: ${detailedMessage.date}\n\n${detailedMessage.textBody || detailedMessage.body}\n\n${attachments}\n\n`;
       }
 
-      api.sendMessage(`Successful! Displaying the latest ${displayLimit} messages. Please check pm or spam in your message request.`, event.threadID);
-      api.sendMessage(messageText, event.senderID);
+      chat.reply(`Successful! Displaying the latest ${displayLimit} Inbox 📥.`);
+      chat.reply(messageText);
+      chat.react("📮");
     } else {
       const count = Math.min(args[0] || 1, MAX_EMAIL_COUNT);
-      if (count > MAX_EMAIL_COUNT) return api.sendMessage(`Maximum allowed count is ${MAX_EMAIL_COUNT}.`, event.threadID);
+      if (count > MAX_EMAIL_COUNT) return chat.reply(`Maximum allowed count is ${MAX_EMAIL_COUNT}.`);
       const generatedEmails = (await axios.get(`${TEMP_MAIL_URL}?action=genRandomMailbox&count=${count}`)).data.map(email => `${email.replace(/\./g, '(.)')}`).join('\n');
       const mail = new TempMail(generateRandomId());
-      api.sendMessage(generatedEmails, event.threadID);
-      api.sendMessage(mail.address, event.threadID);
+      chat.reply(generatedEmails);
+      chat.reply(`Extra: ${mail.address}`);
     }
   } catch (error) {
+    chat.react("⚠️");
     console.error('Error:', error);
-    api.sendMessage("Failed to generate or retrieve email, please try again.", event.threadID);
+    chat.reply("Failed to generate or retrieve email, please try again.");
   }
 };
