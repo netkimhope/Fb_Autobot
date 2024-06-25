de6e80f7c036b385d877098dbbf62f2420d5688e
netkimhope <168548012+netkimhope@users.noreply.github.com>
2024-06-25 22:33:21 +0800

    Update Ai3.js

--- script/Ai3.js
+++ script/Ai3.js
@@ -1,33 +1,87 @@
 const axios = require('axios');
 
+
+
 module.exports.config = {
+
   name: 'ai3',
+
   version: '1.0.0',
+
   role: 0,
+
   hasPrefix: false,
-  aliases: ['ai3'],
-  description: "talk with ai",
-  usage: "Ai [text]",
-  credits: 'H0P3',
+
+  aliases: ['gpt', 'ai3' 'openai'],
+
+  description: "An AI command powered by GPT-4",
+
+  usage: "Ai [promot]",
+
+  credits: 'Developer',
+
   cooldown: 3,
+
 };
 
+
+
 module.exports.run = async function({ api, event, args }) {
+
+
+
   const input = args.join(' ');
+
+
+
+
+
+  
+
   if (!input) {
-    api.sendMessage(`Hello there!\n\nI am here to assist you with any questions or tasks you may have.\n\nusage: ai what is atom?`, event.threadID, event.messageID);
+
+    api.shareContact(`𝑯𝑬𝑳𝑳𝑶 𝑰𝑴 𝑨𝑰 
+
+
+
+━━━━━━━━━━━━━━━
+
+
+
+ 𝑷𝑳𝑬𝑨𝑺𝑬 𝑷𝑹𝑶𝑽𝑰𝑫𝑬 𝑨 𝑸𝑼𝑬𝑺𝑻𝑰𝑶𝑵/𝑸𝑼𝑬𝑹𝒀`,api.getCurrentUserID(), event.threadID, event.messageID);
+
     return;
+
   }
-  api.sendMessage(`Searching...`, event.threadID, event.messageID);
+
+  api.setMessageReaction("⏳", event.messageID, (err) => {
+
+  }, true);
+
+api.sendTypingIndicator(event.threadID, true);
+
+
+
+  api.shareContact(`🔍𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜 𝙋𝙡𝙚𝙖𝙨𝙚 𝙒𝙖𝙞𝙩....
+
+━━━━━━━━━━━━━━━━━━\n\n "${input}"`,api.getCurrentUserID(),event.threadID, event.messageID);
+
+  
+
   try {
-    const { data } = await axios.get(`https://joshweb.click/api/catgpt?prompt=hi%20who%20are%20you?${encodeURIComponent(input)}`);
-    console.log(data); 
-    const response = data.content; 
-    
-    const finalResponse = `𝘼𝙞\n\n${response}`;
-    api.sendMessage(finalResponse + '\n\nhttps://bit.ly/autobot189903', event.threadID, event.messageID);
+
+    const { data } = await axios.get(`https://gpt4o.onrender.com/gpt4o?question=${encodeURIComponent(input)}`);
+
+    let response = response.data;
+
+    response += "\n\n";
+
+    api.shareContact(response,api.getCurrentUserID(), event.threadID, event.messageID);
+
   } catch (error) {
-    api.sendMessage('An error occurred while processing your request, please try sending your question again', event.threadID, event.messageID);
-    console.error(error); 
+
+    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+
   }
+
 };
