ac9c823412b14d7a14db108f2a450075faafafb8
netkimhope <168548012+netkimhope@users.noreply.github.com>
2024-06-27 20:10:13 +0800

    Update goibot.js

--- script/goibot.js
+++ script/goibot.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bot",
 	version: "1.0.0",
 	role: 0,
-	credits: "John Arida",
+	credits: "H0P3",
 	description: "Bot will rep ng tag admin or rep ng tagbot",
 	usages: "",
 	hasPrefix: false,
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, args, Threads }) {
   const time = moment.tz("Asia/Ho_Chi_minh").format("HH:MM:ss L");
   var idgr = `${event.threadID}`;
 
-  var tl = [`Hi this is ChatBotV5 autobot...\n${time}`, `ops What's your need sir?\n${time}`, `Hi, You want to see all commands?Type -help\n${time}`, `misskonasiya\n${time}`, `What can i help you?`, `Hi Sir im here 😊\n${time}`, `tawag ka ng tawag sakin🙄!`, `You're the cutest person on the planet\n${time}`, `Have a nice day User!\n${time}`, `yes?\n${time}`, `yes po?\n${time}`, `hindi ba kayu minahal\n${time}`, `Im here sir\n${time}`, `po?\n${time}`, `bakit po?\n${time}`, `Good luck :)))\n${time}`,`kung ako yan hindi kita gaganyanin...\n${time}`, `wag ako, tawagin mo yung developer ko\n${time}`, `ikaw ang pinaka pogi/ganda sa buong planeta\n${time}`, `wag spam hoi\n${time}`, `hey\n${time}`, `hindi kba minahal\n${time}`];
+  var tl = [`Hi this is xreo'an int. autobot...\n${time}`, `ops What's your need sir?\n${time}`, `Hi, You want to see all commands?Type -help\n${time}`, `misskonasiya\n${time}`, `What can i help you?`, `Hi Sir im here 😊\n${time}`, `tawag ka ng tawag sakin🙄!`, `You're the cutest person on the planet\n${time}`, `Have a nice day User!\n${time}`, `yes?\n${time}`, `yes po?\n${time}`, `hindi ba kayu minahal\n${time}`, `Im here sir\n${time}`, `po?\n${time}`, `bakit po?\n${time}`, `Good luck :)))\n${time}`,`kung ako yan hindi kita gaganyanin...\n${time}`, `wag ako, tawagin mo yung developer ko\n${time}`, `ikaw ang pinaka pogi/ganda sa buong planeta\n${time}`, `wag spam hoi\n${time}`, `hey\n${time}`, `hindi kba minahal\n${time}`];
   var rand = tl[Math.floor(Math.random() * tl.length)]
 
 
