4dee84c2d03dee7ac88af8d570c7d6ff10d7fac0
netkimhope <168548012+netkimhope@users.noreply.github.com>
2024-06-29 07:32:15 +0800

    Add files via upload
    Yeww

--- script/uid.js
+++ script/uid.js
@@ -1,34 +1,17 @@
 module.exports.config = {
-  name: "uid",
-  version: "1.0.0",
-  role: 0,
-  credits: "Hinata",
-  description: "Get User ID.",
-  cooldown: 5,
-  hasPrefix: false,
+	name: "uid",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Hinata",
+	description: "Get User ID.",
+	commandCategory: "other",
+	cooldowns: 5
 };
 
-module.exports.run = async function ({ api, event, args, Users }) {
-  let { threadID, messageID } = event;
-  let uid = event.senderID; // Default to the sender's ID if no specific condition is met
-
-  if (args.length === 0) {
-    // If no arguments provided, default to sender's ID
-    uid = event.senderID;
-  } else if (event.type === "message_reply") {
-    // If the message is a reply, get the sender's ID from the replied message
-    uid = event.messageReply.senderID;
-  } else if (args.join(" ").includes("@")) {
-    // If the message contains a mention (@), get the ID of the mentioned user
-    const mention = args.find(arg => arg.startsWith("@"));
-    if (mention) {
-      const mentionedUserID = Object.keys(event.mentions)[0];
-      if (mentionedUserID) {
-        uid = mentionedUserID;
-      }
-    }
-  }
-
-  // Send the user ID as a message
-  await api.sendMessage(`User ID: ${uid}`, threadID, messageID);
-};
+module.exports.run = function({ api, event, args, Users  }) {
+  let {threadID, senderID, messageID} = event;
+         if (!args[0]) { var uid = senderID}
+  if(event.type == "message_reply") { uid = event.messageReply.senderID }
+  if (args.join(" ").indexOf('@') !== -1){ uid = Object.keys(event.mentions)[0] } 
+	return api.sendMessage(uid, threadID, messageID);
+} 
--- script/unsend.js
+++ script/unsend.js
@@ -1,19 +1,21 @@
 module.exports.config = {
-  name: "unsend",
-  version: "1.0.0",
-  role: 0,
-  hasPrefix: true,
-  aliases: ['unsent', 'remove', 'rm'],
-  usage: 'Unsent [reply]',
-  description: "Unsend bot's message",
-  credits: 'Deveploper',
-  cooldown: 0
+	name: "unsend",
+	version: "1.0.0", 
+	hasPermssion: 2,
+	credits: "hulaanmo",
+	description: "Remove Bot's messages",
+	commandCategory: "System", 
+	usages: "", 
+	cooldowns: 0,
+	dependencies: [] 
 };
-module.exports.run = async function({
-  api,
-  event
-}) {
-  if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage("I can't unsend from other message.", event.threadID, event.messageID);
-  if (event.type != "message_reply") return api.sendMessage("Reply to bot message", event.threadID, event.messageID);
-  return api.unsendMessage(event.messageReply.messageID, err => (err) ? api.sendMessage("Something went wrong.", event.threadID, event.messageID) : '');
-}
+module.exports.languages = { "vi": 
+   { "unsendErr1": "Không thể gỡ tin nhắn của người khác.",
+ "unsendErr2": "Hãy reply tin nhắn cần gỡ." }, 
+"en": { "unsendErr1": "Can't to unsend message from other user.",
+        "unsendErr2": "Reply to the message you want to unsend." } }
+module.exports.run = async function({ api, event, args, Users }) {
+	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText('unsendErr1'), event.threadID, event.messageID);
+			if (event.type != "message_reply") return api.sendMessage(getText('unsendErr2'), event.threadID, event.messageID);
+			return api.unsendMessage(event.messageReply.messageID, err => (err) ? api.sendMessage(getText('error'), event.threadID, event.messageID) : '');
+    }
