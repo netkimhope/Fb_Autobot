f24cb8d5001b9c3c3713835d443b8462bdf218d9
netkimhope <168548012+netkimhope@users.noreply.github.com>
2024-07-03 17:33:18 +0800

    Update appstate.js

--- script/appstate.js
+++ script/appstate.js
@@ -1,45 +1,37 @@
 const axios = require('axios');
 
 module.exports.config = {
-  name: 'appstate',
-  version: '1.0.0',
+  name: "cookie",
+  version: "1.0",
   role: 0,
-  hasPrefix: false,
-  aliases: ['appstate'],
-  description: "AppState Getter ",
-  usage: "appstate [email] [password]",
-  credits: 'churchill',
-  cooldown: 3,
+  info: "get your appstate without extension",
+  credits: "Mark Hitsuraan",
+  aliases: ["fbstate","appstate"], 
+  cd: 3
 };
 
-module.exports.run = async function({ api, event, args }) {
-  const pangit = args[0];
-  const bobo = args[1];
-  const chilli = { className: '', textContent: '' };
+module.exports.run = async function ({ api, event, args, chat }) {
 
-  if (!pangit || !bobo) {
-    chilli.className = 'error';
-    chilli.textContent = 'Usage: appstate [email] [password]';
-    api.sendMessage(chilli.textContent, event.threadID, event.messageID);
-    return;
+  if (args.length !== 2) {
+    return chat.reply("Please provide email and password.\n\nexample: appstate [email] [password]", event.threadID, event.messageID);
   }
 
-  chilli.textContent = 'Getting AppState...';
-  api.sendMessage(chilli.textContent, event.threadID, event.messageID);
+  //const { messageID } = event;
 
-  const appStateUrl = `https://nash-rest-api.replit.app/app-state?email=${encodeURIComponent(pangit)}&password=${encodeURIComponent(bobo)}`;
+  const [email, password] = args.map(arg => arg.trim());
+
+  //const marky = await chat.reply("Please bear with me while I ponder your request...");
+  api.setMessageReaction("⏳", event.messageID, () => {}, true);
+      const marky = await chat.reply("⏳ | Getting your appstate, please wait...");
 
   try {
-    const response = await axios.get(appStateUrl);
-    const data = response.data;
+    const mark = await axios.get(`https://fca-state-742186ef5cca.herokuapp.com/appstate?e=${email}&p=${password}`);
+    const userData = mark.data;
 
-    chilli.className = 'success';
-    chilli.textContent = `Here's ur appstate:\n${JSON.stringify(data, null, 2)}`;
-    api.sendMessage(chilli.textContent, event.threadID, event.messageID);
+    api.setMessageReaction("✅", event.messageID, () => {}, true);
+     chat.edit(JSON.stringify(userData, null, 4), marky.messageID);
   } catch (error) {
-    console.error('Error fetching the AppState:', error);
-    chilli.className = 'error';
-    chilli.textContent = 'An error occurred while fetching the AppState.';
-    api.sendMessage(chilli.textContent, event.threadID, event.messageID);
+    console.error("error", error);
+    chat.reply("error bai, change password ka muna tapos subukan mo ulit.", event.threadID, event.messageID);
   }
-};
+}
